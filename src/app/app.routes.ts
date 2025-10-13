e7cbd58d50032f63f6b7d58f2d8a74dcbe08c50b
Leonardo Mansueto <leonardo.mansueto@infocamere.it>
2025-10-13 14:30:52 +0200

    prima commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './modules/share/not-found/not-found.component';
+
+export const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule) },
+  {
+    path: '404',
+    component: NotFoundComponent,
+    data: { title: 'PAGINANONTROVATA.TITOLO', context: 'private' }
+  },
+  { path: '**', redirectTo: '/404' },
+
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
+
